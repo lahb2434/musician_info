17eb26f9d1d071f2c8747d57be027b0d5ca3a10c
lahb2434 <chrishoughton2434@gmail.com>
2020-12-28 16:03:19 -0500

    Environment

---
+++ lib/musician_info_environ.rb
@@ -0,0 +1,15 @@
+module MusicianInfo
+end
+
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+
+require_relative "musician_info/version"
+require_relative "musician_info/band"
+require_relative "musician_info/album"
+require_relative "musician_info/scraper"
+require_relative "musician_info/member"
+require_relative "musician_info/cli"
+
